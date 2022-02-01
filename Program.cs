e8ee323363bae9f919db4a9f7129be2da4f60310
Ahad Chowdhury <ahad@maacpiash.com>
2022-02-02 09:22:25 +1100

    ✨ Add Swagger UI

--- Program.cs
+++ Program.cs
@@ -22,7 +22,13 @@
  * SOFTWARE.
  */
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
