7a40bfd24db7e408909c8927e16d638f33af7bb9
Ahad Chowdhury <ahad@maacpiash.com>
2022-02-13 01:37:19 +1100

    👔 Add Token Validator
    This would be used for validating refresh tokens.

---
+++ TokenValidator.cs
@@ -0,0 +1,68 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Mohammad Abdul Ahad Chowdhury
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+public class TokenValidator
+{
+	private readonly byte[] _accessTokenSecret;
+	private readonly byte[] _refreshTokenSecret;
+	private readonly string _issuer;
+	private readonly string _audience;
+
+	public TokenValidator(IConfiguration config, ILogger<TokenValidator> logger)
+	{
+		_accessTokenSecret = Encoding.ASCII.GetBytes(config.GetValue<string>("Jwt:AccessTokenSecret"));
+		_refreshTokenSecret = Encoding.ASCII.GetBytes(config.GetValue<string>("Jwt:RefreshTokenSecret"));
+		_issuer = config.GetValue<string>("Jwt:Issuer");
+		_audience = config.GetValue<string>("Jwt:Audience");
+	}
+
+	public Guid ValidateRefreshToken(string refreshToken)
+	{
+		var tokenHandler = new JwtSecurityTokenHandler();
+		var tokenValidationParams = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(_refreshTokenSecret),
+			ValidateIssuer = true,
+			ValidateAudience = true,
+			ClockSkew = TimeSpan.Zero,
+			ValidAudience = _audience,
+			ValidIssuer = _issuer,
+		};
+
+		try
+		{
+			tokenHandler.ValidateToken(refreshToken, tokenValidationParams, out SecurityToken token);
+			var jwt = (JwtSecurityToken)token;
+			return new Guid(jwt.Id);
+		}
+		catch (Exception e)
+		{
+			return Guid.Empty;
+		}
+	}
+}
