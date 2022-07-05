902a587cff689c3ee4607acaf68200f478adb896
Ahad Chowdhury <ahad@maacpiash.com>
2022-07-05 21:50:26 +1000

    🚚 Move Model classes to separate files
    Classes `Todo` and `User` are in separate files in `Models` directory.

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -22,28 +22,6 @@
  * SOFTWARE.
  */
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Identity;
-
-public class User : IdentityUser<Guid>
-{
-	public string FullName { get; set; }
-	public int Age { get; set; }
-	public string Role { get; set; }
-	public string Address { get; set; }
-	public HashSet<Todo> Todos { get; set; }
-
-	public User() { }
-
-	public User(UserCreateDTO dto)
-	{
-		FullName = dto.FullName;
-		Email = dto.Email;
-		UserName = dto.Username;
-		Age = dto.Age;
-		Role = dto.Role;
-		Address = dto.Address;
-	}
-}
 
 public class Todo
 {
